bde2d0321ecd7585f02f8ef5a978d18f3e8768a7
malkitbenning <malkitbenning@gmail.com>
2023-06-13 09:47:25 +0100

    individual country scores sort complete

--- high-score/src/HighScoreTable.js
+++ high-score/src/HighScoreTable.js
@@ -3,6 +3,19 @@ import "./HighScoreTable.css";
 import ScoresBlock from "./ScoresBlock";
 
 const HighScoreTable = (props) => {
+
+  function compare(a, b) {
+    if (a.s > b.s) {
+      return -1;
+    }
+    if (a.s < b.s) {
+      return 1;
+    }
+    return 0;
+  }
+
+  props.aCountryScores.scores.sort(compare);
+
   return (
     <div className="high-score-table">
       <div className="country-line">
